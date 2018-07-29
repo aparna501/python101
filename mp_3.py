13d5bd55a28b8b48923eea7ce5121c835922680d
aparna501 <41412893+aparna501@users.noreply.github.com>
2018-07-29 23:11:38 +0530

    Update mp_3.py

--- mp_3.py
+++ mp_3.py
@@ -3,3 +3,7 @@ string_length=len(alphabet_string)
 reversed_string=alphabet_string[-1:-(string_length+1):-1]
 print(alphabet_string)
 print(reversed_string)
+
+out:
+  enter a string
+  gnirts a retne
