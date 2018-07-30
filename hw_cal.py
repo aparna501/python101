a0a5dfe85ab68f8b1a535c5d54f22357026c880e
aparna501 <aparna.d501@gmail.com>
2018-07-30 21:51:38 +0530

    commit

---
+++ hw_cal.py
@@ -0,0 +1,11 @@
+class Calculator:
+	result=0
+	def__init__(self,a,b):
+		self.a=a
+		self.b=b
+	def.add(self):
+		self.result=self.a+self.b
+		retun self.result
+	def.sub(self):
+		self.result=self.a+self.b
+		return self.result
