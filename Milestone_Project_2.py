b9edb410113982971a90f604fac9e9a8e7b04201
aparna501 <aparna.d501@gmail.com>
2018-08-09 20:32:48 +0530

    commit

--- Milestone_Project_2.py
+++ Milestone_Project_2.py
@@ -1,18 +1,23 @@
 class Stack() :
-
-   def __init__(self) :
-    self.items = [1,2,3,4,5,6,7]
-   def push(self, item) :
-    self.items.append(item)
-
-   def pop(self) :
-    return self.items.pop()
-
-
+	def __init__(self) :
+		self.item = []
+	def push(self, item) :
+		self.item.append(item)
+	def pop(self) :
+		return self.item.pop()
+	def peek(self):
+		return self.item.peek()
 if __name__ == "__main__":
-    demoStack = Stack()
-    demoStack.push(8)
-    demoStack.push(9)
-    demoStack.push(9)
-    demoStack.pop()
-    print(demoStack.items)
+    s1 = Stack()
+    s1.push(1)
+    s1.push(2)
+    s1.push(3)
+    s1.push(4)
+    s1.push(5)
+    s1.push(6)
+    s1.push(7)
+    s1.push(8)
+    s1.push(8)
+    s1.pop()
+print(s1.item)
+
