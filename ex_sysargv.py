6dac2411a8f217fbc3e32cc024959f4129195b74
aparna501 <aparna.d501@gmail.com>
2018-08-10 10:57:46 +0530

    commit

---
+++ ex_sysargv.py
@@ -0,0 +1,15 @@
+#sys module
+import sys
+pgm_name=sys.argv[0]
+print(pgm_name)
+arguments=sys.argv[1:]
+count=len(arguments)
+print(arguments)
+print("this is python")
+
+'''
+input:python ex_sysargv.py
+output:ex_sysargv.py
+[]
+this is python'''
+
